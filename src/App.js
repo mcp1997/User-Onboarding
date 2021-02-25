01f5a2593cdece321071730e4118745f8c110bf9
Matt Phillips <mattcphillips17@gmail.com>
2021-02-24 20:30:44 -0700

    project setup

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import React, { useState, useEffect } from 'react'
+
+const initialFormValues = {
+  // text inputs
+  username: '',
+  email: '',
+  password: '',
+  // checkbox
+  tos: false
+}
+
+const initialFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+  tos: ''
+}
+
+const initialUsers = []
+const initialDisabled = true
+
+function App() {
+  // states
+  const []
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
